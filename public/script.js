aa98156e50f0c70403801c190535cff649c25133
takashi-marumori <76422764+takashi-marumori@users.noreply.github.com>
2021-03-11 11:22:20 +0900

    新規登録、ログインモーダル作成

---
+++ public/script.js
@@ -0,0 +1,19 @@
+$(function(){
+  $('.signup-show').click(function(){
+    $('#signup-modal').fadeIn();
+  });
+  
+  $('#signup-close-modal').click(function(){
+    $('#signup-modal').fadeOut();
+  });
+});
+
+$(function(){
+  $('.login-show').click(function(){
+    $('#login-modal').fadeIn();
+  });
+  
+  $('#login-close-modal').click(function(){
+    $('#login-modal').fadeOut();
+  });
+});
---
+++ public/sign.js
@@ -0,0 +1,14 @@
+new Vue({
+  el:'#app',
+  data: {
+    email: '',
+    password: '',
+    nickname: ''
+  },
+  validations: {
+    email: {
+      required,
+      
+    }
+  }
+})
